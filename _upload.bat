5017eb594cb5253983817152af4b4000aa82fc1d
EnchoDoubutsuen <encho.doubutsuen@gmail.com>
2017-03-20 16:18:11 +0900

    commit by batch

--- _upload.bat
+++ _upload.bat
@@ -1,3 +1,4 @@
 git add --all
 git commit -m "commit by batch"
 git push origin master
+set /p end="Input Enter..."
---
+++ _version.bat
@@ -0,0 +1,3 @@
+git fetch origin
+git diff HEAD..origin/master --stat
+set /p end="Input Enter..."
