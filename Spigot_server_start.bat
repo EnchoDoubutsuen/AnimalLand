30085f33fba504537603ea7aeead759cc4f5c9bd
EnchoDoubutsuen <encho.doubutsuen@gmail.com>
2017-06-09 00:11:23 +0900

    commit by batch

--- Spigot_server_start.bat
+++ Spigot_server_start.bat
@@ -1,3 +1,3 @@
 @echo off
-java -server -Xms3G -Xmx3G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn512M -XX:MaxNewSize=512M -jar spigot-1.11.2.jar 
+java -server -Xms3G -Xmx3G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn512M -XX:MaxNewSize=512M -jar spigot-1.12.jar 
 pause
