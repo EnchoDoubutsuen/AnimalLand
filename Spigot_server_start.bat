3c34c67be5502f71c73cb97f5fb65406c3b1247c
EnchoDoubutsuen <encho.doubutsuen@gmail.com>
2017-07-30 13:10:54 +0900

    commit by batch

--- Spigot_server_start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -server -Xms3G -Xmx3G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn512M -XX:MaxNewSize=512M -jar spigot-1.12.jar 
-pause
---
+++ start_server.bat
@@ -0,0 +1,1 @@
+java -server -Xms3G -Xmx3G -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn512M -XX:MaxNewSize=512M -jar minecraft_server.1.12.jar nogui
